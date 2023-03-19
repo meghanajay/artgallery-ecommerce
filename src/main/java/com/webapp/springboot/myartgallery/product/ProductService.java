84064967884abca6a315cdd178de96bc1f817b37
meghanajay <meghanajay13@gmail.com>
2023-03-19 14:23:55 +0000

    update feature added

--- src/main/java/com/webapp/springboot/myartgallery/product/ProductService.java
+++ src/main/java/com/webapp/springboot/myartgallery/product/ProductService.java
@@ -2,6 +2,7 @@ package com.webapp.springboot.myartgallery.product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,13 @@ public class ProductService {
 
 	public void addProduct(String prodName, String desc, int price) {
 		productRep.save(new ProductInfo(prodName, desc, price));
+	}
+	
+	public Optional<ProductInfo> findById(int id) {
+		return productRep.findById(id);
+	}
+	
+	public void updateProduct(int id, String prodName, String desc, int price) {
+		productRep.save(new ProductInfo(id, prodName, desc, price));
 	}
 }
